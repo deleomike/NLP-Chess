2ca76a04a2bccfa470709b0f2313ef2ee93c22eb
deleomike <michaeldeleo31@yahoo.com>
2022-03-16 19:20:48 -0400

    New nim

--- src/bert/run_nim.py
+++ src/bert/run_nim.py
@@ -2,7 +2,7 @@ from BertHarmon import BertHarmon
 
 model = BertHarmon("./models/bert-nim/", "./models/bert-nim-tokenizer/vocab.txt")
 
-ans = model.pipeline("a3/b5/c4 A [MOVESEP] [MASK]")
+ans = model.pipeline("a0/b3/c3 A [MOVESEP] [MASK]")
 print(ans)
 for i in ans:
-    print(i["token_str"].replace(" ", ""))
+    print(f"{i['token_str'].replace(' ', '')}: {i['score']}")
